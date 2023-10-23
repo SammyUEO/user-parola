bb5701d10a4496e23b14f318c33d4ea69b7e5e78
SammyUEO <samuel.pop@emanuel.ro>
2023-10-23 23:59:35 +0300

    user-parola

---
+++ user-parola/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        const string usernameCorect = "Sammy";
+        const string parolaCorecta = "God is good";
+
+        bool autentificat = false;
+
+        while (!autentificat)
+        {
+            Console.Write("Introduceți username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Introduceți parola: ");
+            string parola = Console.ReadLine();
+
+            if (username == usernameCorect && parola == parolaCorecta)
+            {
+                Console.WriteLine("Bine ai venit, " + usernameCorect);
+
+            }
+            else
+            {
+                Console.WriteLine("Autentificare eșuată. Vă rugăm să încercați din nou.");
+            }
+        }
+    }
+}
